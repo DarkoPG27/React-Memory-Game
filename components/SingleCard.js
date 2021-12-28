3de51030a7d5b98b3587a5658685281caee6e6a3
Darko <89515851+DarkoPG27@users.noreply.github.com>
2021-12-28 01:41:41 +0100

    Flipping Cards
    flipped
    setTimeout on ResetTurn
    rotateY

--- components/SingleCard.js
+++ components/SingleCard.js
@@ -1,12 +1,22 @@
 import './SingleCard.css';
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
+
     return (
 
         <div className="card" >
-            <div>
+            <div className={flipped ? "flipped" : ""}>
                 <img className="front" src={card.src} alt="card front" />
-                <img className="back" src="/img/card-cover.jpeg" alt="card back" />
+                <img
+                    className="back"
+                    src="/img/card-cover.jpeg"
+                    onClick={handleClick}
+                    alt="card back"
+                />
             </div>
         </div>
 
